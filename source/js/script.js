c4dbaefdeaaedbffc28c1390e01d026d563965aa
Дмитрий Сорочкин <ancien@bk.ru>
2023-09-18 21:06:31 +0300

    Изменяет фон блока hero

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,8 @@
 let navMain = document.querySelector('.header-navigation');
-let navToggle = document.querySelector('.header-navigation__toggle');
+let navToggle = document.querySelector('.header__toggle');
 
 navMain.classList.remove('header-navigation--nojs');
+navToggle.classList.remove('header__toggle--nojs');
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('header-navigation--closed')) {
@@ -10,5 +11,13 @@ navToggle.addEventListener('click', function () {
   } else {
     navMain.classList.add('header-navigation--closed');
     navMain.classList.remove('header-navigation--opened');
+  }
+
+  if (navToogle.classList.contains('header__toggle--closed')) {
+    navToogle.classList.remove('header__toggle--closed');
+    navToogle.classList.add('header__toggle--opened');
+  } else {
+    navToogle.classList.add('header__toggle--closed');
+    navToogle.classList.remove('header__toggle--opened');
   }
 });
